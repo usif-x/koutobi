44520d9158c21eb8bb5db3b350b612e33e760c96
usifk <me@usif.tech>
2025-03-04 03:06:30 +0200

    add crisp chat

--- app.vue
+++ app.vue
@@ -5,7 +5,20 @@
   </NuxtLayout>
 </div>
 </template>
-
+<script setup>
+useHead({
+  script: [{
+    innerHTML: `
+      window.$crisp = [];
+      window.CRISP_WEBSITE_ID = "568ba1de-fe8c-41c9-a668-aedf9737dcd6";
+    `,
+    type: 'text/javascript'
+  }, {
+    src: 'https://client.crisp.chat/l.js',
+    async: true
+  }]
+});
+</script>
 <style>
 .page-enter-active,
 .page-leave-active {
