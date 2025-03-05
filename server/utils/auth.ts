9ddf724d1aecaafa4a0a799af1ca526e2cfa3fde
usifk <me@usif.tech>
2025-03-05 22:47:10 +0200

    fix login and add alert

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -8,13 +8,13 @@ export const generateTokens = (
     const accessToken = jwt.sign(
         { userId, tokenVersion, isAdmin },
         process.env.JWT_SECRET!,
-        { expiresIn: '1m' }
+        { expiresIn: '1d' }
     )
 
     const refreshToken = jwt.sign(
         { userId, tokenVersion, isAdmin },
         process.env.JWT_REFRESH_SECRET!,
-        { expiresIn: '2m' }
+        { expiresIn: '7d' }
     )
 
     return { accessToken, refreshToken }
